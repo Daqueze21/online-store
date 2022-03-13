4d6026fc8717b652a8a08fd4b14265afcd502296
Dzmitry Bryt <bdv212@yandex.ru>
2022-03-13 19:09:34 +0300

    feat: create server folder, create models for db, connect server with db

---
+++ server/models/models.js
@@ -0,0 +1,78 @@
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('user', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  status: { type: DataTypes.STRING, defaultValue: 'USER' },
+  name: { type: DataTypes.STRING, allowNull: false },
+  address: { type: DataTypes.STRING, allowNull: true },
+});
+
+const Basket = sequelize.define('basket', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const BasketProducts = sequelize.define('basket_products', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const Products = sequelize.define('products', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  price: { type: DataTypes.INTEGER, allowNull: false },
+  img: { type: DataTypes.STRING, allowNull: false },
+});
+
+const ProductInfo = sequelize.define('product_info', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING, allowNull: false },
+});
+
+const Type = sequelize.define('type', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Brand = sequelize.define('brand', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const TypeBrand = sequelize.define('type_brand', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+Basket.hasMany(BasketProducts);
+BasketProducts.belongsTo(Basket);
+
+Type.hasMany(Products);
+Products.belongsTo(Type);
+
+Brand.hasMany(Products);
+Products.belongsTo(Brand);
+
+Products.hasMany(BasketProducts);
+BasketProducts.belongsTo(Products);
+
+Products.hasMany(ProductInfo, { as: 'info' });
+ProductInfo.belongsTo(Products);
+
+Type.belongsToMany(Brand, { through: TypeBrand });
+Brand.belongsToMany(Type, { through: TypeBrand });
+
+module.exports = {
+  User,
+  Basket,
+  BasketProducts,
+  Products,
+  ProductInfo,
+  Type,
+  Brand,
+  TypeBrand,
+};
